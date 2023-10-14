4ad75be621a34baefa940ba8c2d6c761b7e24222
Ishmeet <144150492+Jugglerish@users.noreply.github.com>
2023-10-14 23:35:08 +0530

    game.py

--- game.py
+++ game.py
@@ -1,20 +1,54 @@
-import pygame, sys
+from grid import Grid
+from blocks import *
+import random
+class Game:
+    def __init__(self):
+        self.grid = Grid()
+        self.blocks = [IBlock(), LBlock(), OBlock(), SBlock(), TBlock()]
+        self.current_block = self.get_random_block()
+        self.next_block = self.get_random_block()
 
-pygame.init()
-dark_blue = (44, 44, 127)
-screen = pygame.display.set_mode((300, 600))
-pygame.display.set_caption("Python Tetris game")
+    def get_random_block(self):
+        if len(self.blocks) > 0:
+            block = random.choice(self.blocks)
+        self.blocks.remove(block)
+        return block
+    
+    def move_left(self):
+        self.current_block.move(0, -1)
+        if self.block_inside() == False:
+            self.current_block.move(0, 1)
 
-clock = pygame.time.Clock()
+    def move_right(self):
+        self.current_block.move(0, 1)
+        if self.block_inside() == False:
+            self.current_block.move(0, -1)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit
-            
-    #Drawing 
-    screen.fill(dark_blue)
-    pygame.display.update()
-    clock.tick(60)
 
+
+    def move_down(self):
+        self.current_block.move(1, 0)
+        if self.block_inside() == False:
+            self.current_block.move(-1, 0)
+    
+    def rotate(self):
+        self.current_block.rotate() 
+        if self.block_inside() == False:
+            self.current_block.undo()
+        
+    def undo(self):
+        self.rotation_state -= 1
+        if self.rotation_state == 0:
+            self.rotation_state = len(self.cells)
+
+
+    def block_inside(self):
+        tiles = self.current_block.get_self_positions()
+        for tile in tiles: 
+            if self.grid.is_inside(tile.row, tile.column) ==  False:
+                return False
+        return True
+
+    def draw(self, screen):
+        self.grid.draw(screen)
+        self.current_block.draw(screen)
